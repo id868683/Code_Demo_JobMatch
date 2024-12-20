75eea8da274611f06787763216ba7f329c1bd7fb
id868683 <82251849+id868683@users.noreply.github.com>
2024-12-20 23:06:11 +0700

    ok

---
+++ JobMatch/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using JobMatch.Data;
+
+namespace JobMatch.Models
+{
+    public class Job
+    {
+       
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Salary { get; set; }
+        public string Image {  get; set; }
+
+        public int CompanyId { get; set; }  //foreign key
+        public Company Company { get; set; }
+
+    }
+}
